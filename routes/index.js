c7ef101f76fe751233387906c03b57f58824fc1e
yuushaka <yu0022yu@gmail.com>
2020-09-14 17:16:51 +0900

    α1.0.9

--- routes/index.js
+++ routes/index.js
@@ -76,7 +76,7 @@ router.get('/api/v1/temperature', function(req, res, next) {
 });
 
 router.get('/api/v1/update',function(req,res,next){
-  exec("git pull", (error, stdout, stderr) => {
+  exec("cd /home/pi/remobooo-pi & git pull", (error, stdout, stderr) => {
     if (error) {
       console.log(error);
       console.log(stderr);
